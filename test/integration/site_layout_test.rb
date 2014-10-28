dff3a8dfa742ecd4cc50363d300a1638b813199c
Alexey-Kh <alexey_kharkin@gmail.com>
2014-10-28 21:03:52 +0300

    Add integration test for layout links for logged-in and non-logged-in users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,35 +4,35 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   def setup
     @non_admin = users(:petya)
-
-    test "layout links as non-logged-in user" do
-      get root_path
-      assert_template 'static_pages/home'
-      assert_select "a[href=?]", root_path, count: 2
-      assert_select "a[href=?]", help_path
-      assert_select "a[href=?]", login_path
-      assert_select "a[href=?]", signup_path
-      assert_select "a[href=?]", contact_path
-      assert_select "a[href=?]", contact_path
-      assert_select "a[href=?]", about_path
-      assert_select "a[href=?]", contact_path
-      get signup_path
-      assert_select "title", full_title("Sign up")
-    end
-
-    test "layout links as logged-in user" do
-      #log_in_as(@non_admin)
-      get root_path
-      assert_template 'static_pages/home'
-      assert_select "a[href=?]", root_path, count: 2
-      assert_select "a[href=?]", help_path
-      assert_select "a[href=?]", users_path
-      assert_select "a[href=?]", user_path(@non_admin)
-      assert_select "a[href=?]", edit_user_path(@non_admin)
-      assert_select "a[href=?]", logout_path
-      assert_select "a[href=?]", about_path
-      assert_select "a[href=?]", contact_path
-    end
-
   end
+
+  test "layout links as non-logged-in user" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links as logged-in user" do
+    log_in_as(@non_admin)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@non_admin)
+    assert_select "a[href=?]", edit_user_path(@non_admin)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+   end
+  
 end
