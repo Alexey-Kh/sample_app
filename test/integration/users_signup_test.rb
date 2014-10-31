a6ab4ef17d3aa838c09ffc92603834db413b6cfb
Alexey-Kh <alexey_kharkin@gmail.com>
2014-10-31 23:48:17 +0300

    Improve users_signup_test at line 41

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -38,10 +38,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     log_in_as(user)
     assert_not is_logged_in?
     # Invalid activation token
-    get edit_account_activation_path('invalid token')
+    get edit_account_activation_path('invalid token', email: user.email)
     assert flash[:danger] == "Invalid activation link"
     assert_not is_logged_in?
-
     # Valid token, wrong email
     get edit_account_activation_path(user.activation_token, email: 'wrong')
     assert_not is_logged_in?
